c42fa1993bc1ecc098cd2d9f5cafea4221ea445f
kartik <kartik.aggarwal@stu.bmcc.cuny.edu>
2020-05-03 15:40:32 -0400

    Initial commit

---
+++ src/main/java/com/picturesque/profile/controller/health/HealthChecker.java
@@ -0,0 +1,18 @@
+package com.picturesque.profile.controller.health;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthChecker {
+
+    @RequestMapping("/v1")
+    @ResponseStatus(HttpStatus.OK)
+    public Healthy healthCheck() {
+        return new Healthy(1, "profile system is up");
+    }
+
+}
