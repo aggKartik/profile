4c4ad6a767df777f75ec39c40f2af7d93dfdf537
kartik <kartik.aggarwal@stu.bmcc.cuny.edu>
2020-05-03 16:19:34 -0400

    updated health check routing

--- src/main/java/com/picturesque/profile/controller/health/HealthChecker.java
+++ src/main/java/com/picturesque/profile/controller/health/HealthChecker.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping("/v1")
 public class HealthChecker {
 
-    @RequestMapping("/v1")
+    @RequestMapping("/health")
     @ResponseStatus(HttpStatus.OK)
     public Healthy healthCheck() {
         return new Healthy(1, "profile system is up");
