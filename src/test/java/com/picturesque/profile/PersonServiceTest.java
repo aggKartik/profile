1162f529c4a68291171ad2bc2eee8e43801cc31d
Kartik <kartik.aggarwal@stu.bmcc.cuny.edu>
2020-05-27 22:52:59 -0400

    Add remaining Person tests

--- src/test/java/com/picturesque/profile/PersonServiceTest.java
+++ src/test/java/com/picturesque/profile/PersonServiceTest.java
@@ -6,6 +6,7 @@ import com.picturesque.profile.exceptions.PersonIllegalArgument;
 import com.picturesque.profile.helperModels.UserID;
 import com.picturesque.profile.payloads.GenericResponse.Response;
 import com.picturesque.profile.payloads.POSTRequests.PersonRequest;
+import com.picturesque.profile.payloads.PUTRequests.PersonPutRequest;
 import com.picturesque.profile.payloads.PersonAddResponse;
 import com.picturesque.profile.repos.PersonMDRepository;
 import com.picturesque.profile.repos.PersonRepository;
@@ -13,6 +14,7 @@ import com.picturesque.profile.service.PersonService;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -91,30 +93,94 @@ public class PersonServiceTest {
   @Test
   void allRestrictionsTest() {
 
-    req.setUserName("---JohnDoe---");
+    req.setUserName("     ---JohnDoe---");
     given(personRepository.findByUserName(req.userName)).willReturn(null);
 
-    try {
-      personService.addPerson(req);
-    } catch (PersonIllegalArgument e) {
-      assertThat(e.getMessage(), equalTo("Illegal username specified"));
-    }
-    try {
-      req.setUserName("JohnDoe");
-      req.setName("                                                                          " +
-              "                                                                              ");
-      personService.addPerson(req);
-    } catch (PersonIllegalArgument e) {
-      assertThat(e.getMessage(), equalTo("Invalid name specified"));
-    }
+    assertThrows(PersonIllegalArgument.class, () -> personService.addPerson(req),
+            "Illegal username specified");
+    req.setUserName("JohnDoe");
+    req.setName("                                                                          " +
+            "                                                                              ");
+    assertThrows(PersonIllegalArgument.class, () -> personService.addPerson(req),
+            "Illegal name specified");
 
     given(personRepository.findByUserName(req.userName)).willReturn(addedPerson);
+    assertThrows(PersonIllegalArgument.class, () -> personService.addPerson(req),
+            "User with this name already exists!");
+  }
 
-    try {
-      personService.addPerson(req);
-    } catch (PersonIllegalArgument e) {
-      assertThat(e.getMessage(), equalTo("User with this name already exists!"));
-    }
+  @Test
+  void changePersonTest() {
+
+    PersonPutRequest putRequest = new PersonPutRequest("JohnDoe", "Life's Good",
+            "xyz.jpg", new Date(946688400));
+
+    given(personRepository.findByUserName(req.userName)).willReturn(addedPerson);
+    given(personMDRepository.findByUserId(addedPerson.getUserID())).willReturn(metaData);
+
+    Response<PersonAddResponse> resp = personService.changePerson(putRequest);
+
+    assertThat(resp.getStatusCode(), equalTo(HttpStatus.OK));
+    assertThat(resp.getResponse().getMessage(),
+            equalTo("[DOB modified successfully, Bio added successfully!]"));
+
+  }
+
+  @Test
+  void changeJustBioTest() {
+    PersonPutRequest putRequest = new PersonPutRequest("JohnDoe", "Life's Good",
+            null, null);
+
+    given(personRepository.findByUserName(req.userName)).willReturn(addedPerson);
+    given(personMDRepository.findByUserId(addedPerson.getUserID())).willReturn(metaData);
+
+    Response<PersonAddResponse> resp = personService.changePerson(putRequest);
+
+    assertThat(resp.getStatusCode(), equalTo(HttpStatus.OK));
+    assertThat(resp.getResponse().getMessage(),
+            equalTo("[Bio added successfully!]"));
+  }
+
+  @Test
+  void changeJustDobTest() {
+    PersonPutRequest putRequest = new PersonPutRequest("JohnDoe", null,
+            null, new Date(946688400));
+
+    given(personRepository.findByUserName(req.userName)).willReturn(addedPerson);
+    given(personMDRepository.findByUserId(addedPerson.getUserID())).willReturn(metaData);
+
+    Response<PersonAddResponse> resp = personService.changePerson(putRequest);
+
+    assertThat(resp.getStatusCode(), equalTo(HttpStatus.OK));
+    assertThat(resp.getResponse().getMessage(),
+            equalTo("[DOB modified successfully]"));
+  }
+
+  @Test
+  void changeNothingTest() {
+    PersonPutRequest putRequest = new PersonPutRequest("JohnDoe", null,
+            null, null);
+
+    given(personRepository.findByUserName(req.userName)).willReturn(addedPerson);
+    given(personMDRepository.findByUserId(addedPerson.getUserID())).willReturn(metaData);
+
+    Response<PersonAddResponse> resp = personService.changePerson(putRequest);
+
+    assertThat(resp.getStatusCode(), equalTo(HttpStatus.OK));
+    assertThat(resp.getResponse().getMessage(),
+            equalTo("[Nothing was modified!]"));
+  }
+
+  @Test
+  void changePersonNotInDBTest() {
+
+    PersonPutRequest putRequest = new PersonPutRequest("JohnDoe", null,
+            null, null);
+
+    given(personRepository.findByUserName(req.userName)).willReturn(null);
+
+    assertThrows(PersonIllegalArgument.class, () -> personService.changePerson(putRequest),
+            "Person does not exist in the database");
 
   }
 
